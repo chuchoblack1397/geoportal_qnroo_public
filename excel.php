7bf6fdd3a19b8efb2bd99d352c54b2b2f2710f6b
Miguel Parra <shragath@gmail.com>
2020-04-07 16:06:20 -0500

    Borrar archivo xlsx leido

--- excel.php
+++ excel.php
@@ -10,57 +10,81 @@ if (isset($_SESSION)) {
     header('Location: ' . $base_url);
 }
 
-$file = $_POST['archivoExcel'];
-var_dump($file);
+$file = $_FILES['archivoExcel'];
+$FileExtension = pathinfo($file['name']);
+var_dump($file, '</br>');
+var_dump($FileExtension['extension'], '</br>');
+$path = "uploads/";
 
-try {
-    $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReaderForFile("ORDEN DE DOCUMENTOS.xlsx");
-    $reader->setReadDataOnly(true);
-    $spreadsheet = $reader->load("ORDEN DE DOCUMENTOS.xlsx");
-    $worksheet = $spreadsheet->getActiveSheet();
-    $rows = $worksheet->toArray();
-    $highestRow = $worksheet->getHighestRow();
-    $highestColumn = $worksheet->getHighestColumn();
-    $highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn);
-} catch (Exception $e) {
-    echo "Fallo en encontrar archivo xlsx\n" . $e;
+if (file_exists($path . $file['name'])) {
+    echo "Deleting old file.</br>";
+    unlink($path . $file['name']);
 }
 
-var_dump($highestColumnIndex, $highestRow, '</br>');
-$arrayData = array();
-for ($r = 1; $r <= $highestRow; $r++) {
-    for ($c = 2; $c <= $highestColumnIndex; $c++) {
-        $value = $worksheet->getCellByColumnAndRow($c, $r)->getValue();
-        if (isset($value)) {
-            if ($c == 2) $datos["clave_cata"] = $value;
-            if ($c == 3) $datos["nombre_loc"] = $value;
-            if ($c == 4) $datos["direccion"] = $value;
-            if ($c == 5) $datos["documento_estatus"] = $value;
-            if ($c == $highestColumnIndex) {
-                array_push($arrayData, $datos);
+if (move_uploaded_file($file['tmp_name'], $path . $file['name'])) {
+    echo "The file " . basename($file["name"]) . " has been uploaded.";
+} else {
+    echo "Sorry, there was an error uploading your file.";
+}
+
+if ($FileExtension['extension'] == 'xlsx') {
+    try {
+        $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReaderForFile($path . $file['name']);
+        $reader->setReadDataOnly(true);
+        $spreadsheet = $reader->load($path . $file['name']);
+        $worksheet = $spreadsheet->getActiveSheet();
+        $rows = $worksheet->toArray();
+        $highestRow = $worksheet->getHighestRow();
+        $highestColumn = $worksheet->getHighestColumn();
+        $highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn);
+    } catch (Exception $e) {
+        echo "Fallo en encontrar archivo xlsx\n" . $e;
+    }
+
+    var_dump($highestColumnIndex, $highestRow, '</br>');
+    $arrayData = array();
+    for ($r = 1; $r <= $highestRow; $r++) {
+        for ($c = 2; $c <= $highestColumnIndex; $c++) {
+            $value = $worksheet->getCellByColumnAndRow($c, $r)->getValue();
+            if (isset($value)) {
+                if ($c == 2) $datos["clave_cata"] = $value;
+                if ($c == 3) $datos["nombre_loc"] = $value;
+                if ($c == 4) $datos["direccion"] = $value;
+                if ($c == 5) $datos["documento_estatus"] = $value;
+                if ($c == $highestColumnIndex) {
+                    array_push($arrayData, $datos);
+                }
+            } else {
+                break;
             }
-        } else {
-            break;
         }
     }
-}
 
-unset($arrayData[0]);
+    unlink($path . $file['name']);
+    unset($arrayData[0]);
 
-// var_dump($arrayData[0], '</br>');
-var_dump(count($arrayData), '</br>');
+    var_dump("Deleting file already readed", '</br>');
+    var_dump(count($arrayData), '</br>');
 
-echo '<h3>Data</h3>';
-echo '<table>';
-foreach ($arrayData as $row) {
-    echo '<tr>';
-    foreach ($row as $key => $value) {
-        echo '<td>' . $value . '</td>';
+    echo '<h3>Data</h3>';
+    echo '<table>';
+    foreach ($arrayData as $row) {
+        echo '<tr>';
+        foreach ($row as $key => $value) {
+            echo '<td>' . $value . '</td>';
+        }
+        echo '</tr>';
     }
-    echo '</tr>';
+    echo '</table>';
+
+
+
+    //return header('Location: ' . $base_url);
+} else {
+    echo 'Extensión del archivo incorrecta';
+    echo '<script>
+    setTimeout(function () {
+        window.location.href = "";
+    }, 5000);
+</script>';
 }
-echo '</table>';
-
-
-
-return header('Location: ');
