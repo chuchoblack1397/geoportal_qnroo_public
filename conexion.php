6a77e38ac7a45b88cd0906079687752a150e257e
Miguel Parra <shragath@gmail.com>
2020-04-15 21:00:39 -0500

    Agregar a gitignore

--- conexion.php
+++ conexion.php
@@ -1,19 +1,17 @@
 <?php
-	
-	echo "<script>console.log('Dentro archivo conexion');</script>";
 
-	//$host        = "host = localhost";144.91.126.153
-	$host        = "host = 144.91.126.153";
-	$port        = "port = 5432";
-	$dbname      = "dbname = opb_geoportal_00";
-	$credentials = "user = geoadmin password = Overkill23004";
- 
-	$conexion = pg_connect( "$host $port $dbname $credentials"  );
-	if(!$conexion) {
-	 echo 'Conexion Fallida : ', pg_connect_error();
-	 exit();
-	} else {
-	   echo "<script>console.log('Abriendo conexion');</script>";
-	}
- 
-?>
+echo "<script>console.log('Dentro archivo conexion');</script>";
+
+//$host        = "host = localhost";
+$host        = "host = localhost";
+$port        = "port = 5432";
+$dbname      = "dbname = test_opb_geoportal_qroo";
+$credentials = "user = postgres password = Miguel290497*";
+
+$conexion = pg_connect("$host $port $dbname $credentials");
+if (!$conexion) {
+	echo 'Conexion Fallida : ', pg_connect_error();
+	exit();
+} else {
+	echo "<script>console.log('Abriendo conexion');</script>";
+}
