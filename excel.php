892e02d819fd35fbae5383a008613ca536120d89
Miguel Parra <shragath@gmail.com>
2020-05-20 20:23:10 -0500

    Respuesta en ymagomundi actualizar de archivo excel

--- excel.php
+++ excel.php
@@ -33,7 +33,6 @@ if ($FileExtension['extension'] == 'xlsx') { // Valida extension
 
     try { // Inicio funcion para leer excel
         $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReaderForFile($path . $file['name']);
-        //$reader->setReadDataOnly(true);
         $spreadsheet = $reader->load($path . $file['name']);
         $worksheet = $spreadsheet->getActiveSheet();
 
@@ -49,7 +48,8 @@ if ($FileExtension['extension'] == 'xlsx') { // Valida extension
     var_dump($highestColumnIndex, $highestRow);
     $dataArray = array();
     $datos = array();
-    $update = 0;
+    $updatesS = 0;
+    $updatesE = 0;
     //Inicializacion de array para guardar datos    
     $lic_cve__1 = "";
     $queryArray = array();
@@ -81,11 +81,12 @@ if ($FileExtension['extension'] == 'xlsx') { // Valida extension
         $query = 'UPDATE opb_licencias_202004_dvp_32616_u SET lic_num_li = $1, lic_fecha = $2, lic_num_es = $3, lic_anno = $4, lic_movimi = $5, lic_estatu = $6, lic_nom_co = $7, lic_cve_ca = $8, lic_cve__1 = $9, lic_direcc = $10, lic_nom_pr = $11, lic_ap_mat = $12, lic_ap_pat = $13, lic_tipo = $14 WHERE clave_cata = $9';
         $res = pg_query_params($conexion, $query, $datos);
         if ($res) {
-            $update++;
+            $updatesS++;
         } else {
             echo "User must have sent wrong inputs\n fila: $r";
             echo "</br>" . pg_last_error($conexion);
             var_dump($datos);
+            $updatesE++;
         }
         $datos = array();
     }
@@ -95,172 +96,9 @@ if ($FileExtension['extension'] == 'xlsx') { // Valida extension
     $time = microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"];
     echo "<h3>Total updates: $update</h3>";
     echo "<h3>Tiempo transcurrido updates: $time</h3>";
-    //unset($arrayData[0]); //Borra valores del primera posicion de array (Nombres de columnas)
+    $_SESSION["updates"] = ["error" => $updatesE, "success" => $updatesS];
 
-    /* foreach ($arrayData as $row) {
-        foreach ($row as $column => $value) {
-            array_push($setArray, "$column = '$value'");
-        }
-        var_dump($row);
-        $lic_cve__1 = $row['lic_cve__1'];
-        $where = "clave_cata = '$lic_cve__1'";
-        $set = implode(", ", $setArray);
-        $query = "UPDATE opb_licencias_202004_dvp_32616_u SET $set WHERE $where;";
-        var_dump($where);
-        array_push($queryArray, $query);
-        $setArray = array();
-        $res = pg_update($conexion, 'opb_licencias_202004_dvp_32616_u', $row, $where);
-        if ($res) {
-            echo "Data is updated: $res\n";
-        } else {
-            echo "User must have sent wrong inputs\n";
-        }
-    } */
-
-
-
-    /* if (!pg_connection_busy($conexion)) {
-        $res = pg_send_query($conexion, $statements);;
-    }
-    echo '</br></br><h3>Data</h3>';
-    if ($res) {
-        echo "</br>Data is updated: $res\n";
-    } else {
-        echo "</br>User must have sent wrong inputs\n";
-    } */
-
-    //Data
-
-
-    /* echo '<table>';
-    echo '<tr>';
-    foreach ($arrayData[0] as $key => $value) {
-
-        echo '<th>' . $key . '</th>';
-    }
-    echo '</tr>';
-    foreach ($arrayData as $row) {
-        echo '<tr>';
-        foreach ($row as $key => $value) {
-            echo '<td>' . $value . '</td>';
-        }
-        echo '</tr>';
-    }
-    echo '</table>'; */
-
-    //Debbug Nombre columnas
-    /* echo '<h3>Data</h3>';
-    echo '<table>';
-    
-    echo '</table>'; */
-
-    /* echo '<h3>Direccion</h3>';
-    echo '<table>';
-    foreach ($arrayData as $key => $value) {
-        echo '<tr>';
-        $direccionArray = explode(',', $value["direccion"]);
-        foreach ($direccionArray as $key2 => $value2) {
-            echo '<td>' . $value2 . '</td>';
-        }
-
-        echo '</tr>';
-    }
-    echo '</table>'; */
-
-    /* echo '<script>
-    setTimeout(function () {
-        window.location.href = "' . $base_url . '";
-    }, 5000);
-</script>'; */
-
-    //return header('Location: ' . $base_url);
+    return header('Location: ' . $base_url);
 } else {
-    echo 'Extensión del archivo incorrecta';
-    /* echo '<script>
-    setTimeout(function () {
-        window.location.href = "' . $base_url . '";
-    }, 2);
-</script>'; */
+    return header('Location: ' . $base_url);
 }
-
-
-/* $nombres = ["lic_num_li", "lic_fecha", "lic_num_es", "lic_anno", "lic_movimi", "lic_estatu", "lic_nom_co", "lic_cve_ca", "lic_cve__1", "lic_direcc", "lic_nom_pr", "lic_ap_mat", "lic_ap_pat", "lic_tipo"];
-    for ($r = 2; $r <= 4; $r++) { //Filas $highestRow
-        for ($c = 1; $c <= $highestColumnIndex; $c++) { //Columnas 
-            $value = $worksheet->getCellByColumnAndRow($c, $r)->getFormattedValue(); //Obtiene valor de la celda en columna y fila            
-            if (isset($value)) { //Guarda valores de celdas que no sean nulas de lo contrario salta a siguiente fila
-                $value = ltrim($value); //Quita espacios lado izquierdo de la String
-                $value = rtrim($value); //Quita espacios lado derecho de la String
-                if ($nombres[$c - 1] == "lic_fecha") {
-                    $datos[$nombres[$c - 1]] = date("Y-m-d", strtotime($value));
-                    var_dump($value);
-                } else {
-                    $datos[$nombres[$c - 1]] = $value; //Agrega el valor
-                }
-            } else {
-                break; //Siguiente fila
-            }
-        }
-        array_push($arrayData, $datos);
-        $datos = array();
-    } */
-
-
-    //Inicializacion de array para guardar datos    
-    /* $count = 0;
-    $lic_cve__1 = "";
-    $queryArray = array();
-    $setArray = array();
-    $nombres = ["lic_num_li", "lic_fecha", "lic_num_es", "lic_anno", "lic_movimi", "lic_estatu", "lic_nom_co", "lic_cve_ca", "lic_cve__1", "lic_direcc", "lic_nom_pr", "lic_ap_mat", "lic_ap_pat", "lic_tipo"];
-    for ($r = 2; $r <= $highestRow; $r++) { //Filas $highestRow
-        for ($c = 1; $c <= $highestColumnIndex; $c++) { //Columnas 
-            $value = $worksheet->getCellByColumnAndRow($c, $r)->getFormattedValue(); //Obtiene valor de la celda en columna y fila            
-            if (isset($value)) { //Guarda valores de celdas que no sean nulas de lo contrario salta a siguiente fila
-                $value = ltrim($value); //Quita espacios lado izquierdo de la String
-                $value = rtrim($value); //Quita espacios lado derecho de la String
-                if ($nombres[$c - 1] == "lic_fecha") {
-                    $date = date("Y-m-d", strtotime($value));
-                    array_push($setArray, $nombres[$c - 1] . " = '$date'");
-                } else {
-                    //Agrega el valor
-                    array_push($setArray, $nombres[$c - 1] . " = '$value'");
-                }
-                if ($nombres[$c - 1] == 'lic_cve__1') {
-                    $lic_cve__1 = $value;
-                }
-            } else {
-                break; //Siguiente fila
-            }
-        }
-        $datos = array();
-
-        $where = "clave_cata = '$lic_cve__1'";
-        $set = implode(", ", $setArray);
-        $query = "UPDATE opb_licencias_202004_dvp_32616_u SET $set WHERE $where;";
-        //var_dump($where);
-        array_push($queryArray, $query);
-        $setArray = array();
-
-        if ($r % 100 == 0 || $r == $highestRow) {
-            if (!pg_connection_busy($conexion)) {
-                $statements = implode(" ", $queryArray);
-                $res = pg_send_query($conexion, $statements);;
-                echo '<h3>Data</h3>';
-                echo ("Cuenta: " . $r);
-
-                while ($result = pg_get_result($conexion)) {
-                    $results[] = $result;
-                    echo ("</br>Results: " . $result);
-                }
-
-                //echo ($statements);
-            }
-            if ($res) {
-                echo "</br>Data is updated: $res\n";
-            } else {
-                echo "</br>User must have sent wrong inputs\n";
-            }
-            $queryArray = array();
-            $statements = "";
-        }
-    } */
