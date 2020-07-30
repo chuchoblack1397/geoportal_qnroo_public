83e3a3446f08e9d0cab08e69dba056c073b8b98e
Miguel Parra <shragath@gmail.com>
2020-07-30 04:07:51 -0500

    Implementacion de subir imagen georreferenciada, leer exif y formar punto

--- usuario/js_ediciones.js
+++ usuario/js_ediciones.js
@@ -1,80 +1,83 @@
-function cargar_modal_usuario(){
-    var tipo_busqueda="usuario";
-    var ruta="tipo="+tipo_busqueda;
+function cargar_modal_usuario() {
+    var tipo_busqueda = 'usuario';
+    var ruta = 'tipo=' + tipo_busqueda;
 
-    var cuerpoModal_editar_usuario=document.getElementById("cuerpoModal_editar_usuario");
+    var cuerpoModal_editar_usuario = document.getElementById(
+        'cuerpoModal_editar_usuario'
+    );
     $.ajax({
-        url:"php_ediciones.php",
-        type:"POST",
+        url: 'php_ediciones.php',
+        type: 'POST',
         data: ruta,
-        beforeSend:function(){
-            cuerpoModal_editar_usuario.innerHTML="";
-            $('#loader_usuario').show();//mostrar LOADER
+        beforeSend: function () {
+            cuerpoModal_editar_usuario.innerHTML = '';
+            $('#loader_usuario').show(); //mostrar LOADER
         },
-        success: function(res){
-            cuerpoModal_editar_usuario.innerHTML=res;
-            $('#loader_usuario').hide();//ocultar LOADER
+        success: function (res) {
+            cuerpoModal_editar_usuario.innerHTML = res;
+            $('#loader_usuario').hide(); //ocultar LOADER
         },
-        error: function(){
-            alert( "Error con el servidor" );
-        } 
+        error: function () {
+            alert('Error con el servidor');
+        },
     });
-}// fin metodo cargar_modal_usuario
+} // fin metodo cargar_modal_usuario
 
-function actualizar_password(){
-    var tipo_busqueda_password="password";
-    
+function actualizar_password() {
+    var tipo_busqueda_password = 'password';
 
-    var passActual = document.getElementById("passActual").value;
-    var passNueva = document.getElementById("passNueva").value;
-    var passConfirmar = document.getElementById("passConfirmar").value;
+    var passActual = document.getElementById('passActual').value;
+    var passNueva = document.getElementById('passNueva').value;
+    var passConfirmar = document.getElementById('passConfirmar').value;
 
     passActual = passActual.trim();
     passNueva = passNueva.trim();
     passConfirmar = passConfirmar.trim();
 
-    if((passActual=="") || (passNueva=="") || (passConfirmar==""))
-    {
+    if (passActual == '' || passNueva == '' || passConfirmar == '') {
         swal('Faltan campos', 'Por favor rellena todos los campos', 'info');
         return;
-    }//fin if
+    } //fin if
 
-    if(passNueva == passActual)
-    {
+    if (passNueva == passActual) {
         swal('Cuidado', 'Estas usando la misma contraseña', 'warning');
         return;
-    }//fin if
+    } //fin if
 
-    if(passNueva != passConfirmar)
-    {
+    if (passNueva != passConfirmar) {
         swal('Ups', 'La confirmacion no coincide', 'info');
         return;
-    }//fin if
+    } //fin if
 
+    var ruta_password =
+        'tipo=' +
+        tipo_busqueda_password +
+        '&passActual=' +
+        passActual +
+        '&passNueva=' +
+        passNueva;
 
-    var ruta_password="tipo="+tipo_busqueda_password+"&passActual="+passActual+"&passNueva="+passNueva;
-
-    var actualizando = document.getElementById("actualizando");
+    var actualizando = document.getElementById('actualizando');
 
     swal({
-        title: "Advertencia",
-        text: "Estás a punto de cambiar tu contraseña. ¿Estás seguro de hacerlo?",
-        icon: "warning",
+        title: 'Advertencia',
+        text:
+            'Estás a punto de cambiar tu contraseña. ¿Estás seguro de hacerlo?',
+        icon: 'warning',
         buttons: true,
         dangerMode: true,
-        })
-        .then((willDelete) => {
+    }).then((willDelete) => {
         if (willDelete) {
             $.ajax({
-                url:"php_ediciones.php",
-                type:"POST",
+                url: 'php_ediciones.php',
+                type: 'POST',
                 data: ruta_password,
-                beforeSend:function(){
-                    actualizando.innerHTML="<center><span>Actualizando...</span></center>";
+                beforeSend: function () {
+                    actualizando.innerHTML =
+                        '<center><span>Actualizando...</span></center>';
                 },
-                success: function(res){
-                    if(res == "ok")
-                    {
+                success: function (res) {
+                    if (res == 'ok') {
                         /*
                         swal('Perfecto', 'Se han actualizado los datos', 'success');
                         $("#passActual").val('');
@@ -83,110 +86,185 @@ function actualizar_password(){
                         actualizando.innerHTML="";
                         $('#btn_cerrarModalPassoword').click();*/
                         swal({
-                            title: "Perfecto",
-                            text: "Se han actualizado los datos",
-                            icon: "success",
+                            title: 'Perfecto',
+                            text: 'Se han actualizado los datos',
+                            icon: 'success',
                             buttons: true,
                             dangerMode: true,
-                            })
-                            .then((willDelete) => {
-                                if (willDelete) {
-                                    location.href ="../cerrarSesion.php";
-                                
-                                } else {
-                                    location.href ="../cerrarSesion.php";
-                                }
-                            });
+                        }).then((willDelete) => {
+                            if (willDelete) {
+                                location.href = '../cerrarSesion.php';
+                            } else {
+                                location.href = '../cerrarSesion.php';
+                            }
+                        });
                     }
-                    if(res == "error")
-                    {
+                    if (res == 'error') {
                         swal('Error', 'No se realizaron los cambios', 'error');
-                        $("#passActual").val('');
-                        $("#passNueva").val('');
-                        $("#passConfirmar").val('');
-                        actualizando.innerHTML="";
+                        $('#passActual').val('');
+                        $('#passNueva').val('');
+                        $('#passConfirmar').val('');
+                        actualizando.innerHTML = '';
                         $('#btn_cerrarModalPassoword').click();
                     }
-                    if(res == "error_e")
-                    {
+                    if (res == 'error_e') {
                         swal('Error', 'Error en la contraseña', 'error');
-                        actualizando.innerHTML="";
+                        actualizando.innerHTML = '';
                     }
                 },
-                error: function(){
-                    alert( "Error con el servidor" );
-                } 
-            });//fin ajax
-        }//fin if
+                error: function () {
+                    alert('Error con el servidor');
+                },
+            }); //fin ajax
+        } //fin if
         else {
-            $("#passActual").val('');
-            $("#passNueva").val('');
-            $("#passConfirmar").val('');
-            actualizando.innerHTML="";
+            $('#passActual').val('');
+            $('#passNueva').val('');
+            $('#passConfirmar').val('');
+            actualizando.innerHTML = '';
             $('#btn_cerrarModalPassoword').click();
-        }//fin else
-        });
-}// fin metodo cargar_modal_password
+        } //fin else
+    });
+} // fin metodo cargar_modal_password
 
-function actualizar_usuario(){
-    var correo = document.getElementById("correoUser").value;
-    var nombre = document.getElementById("nombreUser").value;
-    var a_paterno = document.getElementById("apUser").value;
-    var a_materno = document.getElementById("amUser").value;
-    var puesto = document.getElementById("puestoUser").value;
-    var privilegio = document.getElementById("privilegioUser").value;
+function actualizar_usuario() {
+    var correo = document.getElementById('correoUser').value;
+    var nombre = document.getElementById('nombreUser').value;
+    var a_paterno = document.getElementById('apUser').value;
+    var a_materno = document.getElementById('amUser').value;
+    var puesto = document.getElementById('puestoUser').value;
+    var privilegio = document.getElementById('privilegioUser').value;
 
-    if((correo != "") && (nombre!="") && (a_paterno!="") && (a_materno!="") && (puesto!="NA") && (privilegio!="NA"))
-    {
+    if (
+        correo != '' &&
+        nombre != '' &&
+        a_paterno != '' &&
+        a_materno != '' &&
+        puesto != 'NA' &&
+        privilegio != 'NA'
+    ) {
         nombre = nombre.trim();
         a_paterno = a_paterno.trim();
         a_materno = a_materno.trim();
-        
-        if (/^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/.test(correo)){
-            document.getElementById("correoUser").className = "form-control";
+
+        if (
+            /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/.test(
+                correo
+            )
+        ) {
+            document.getElementById('correoUser').className = 'form-control';
         } else {
-            document.getElementById("correoUser").className = "form-control is-invalid";
+            document.getElementById('correoUser').className =
+                'form-control is-invalid';
             return;
         }
 
-        var editar_usuario=document.getElementById("cuerpoModal_editar_usuario");
-        var tipo_busqueda_editar="actualizar_usuario";
-        var ruta_editar="tipo="+tipo_busqueda_editar+"&correo="+correo+"&nombre="+nombre+"&ap="+a_paterno+"&am="+a_materno+"&puesto="+puesto+"&privilegio="+privilegio;
+        var editar_usuario = document.getElementById(
+            'cuerpoModal_editar_usuario'
+        );
+        var tipo_busqueda_editar = 'actualizar_usuario';
+        var ruta_editar =
+            'tipo=' +
+            tipo_busqueda_editar +
+            '&correo=' +
+            correo +
+            '&nombre=' +
+            nombre +
+            '&ap=' +
+            a_paterno +
+            '&am=' +
+            a_materno +
+            '&puesto=' +
+            puesto +
+            '&privilegio=' +
+            privilegio;
         $.ajax({
-            url:"php_ediciones.php",
-            type:"POST",
+            url: 'php_ediciones.php',
+            type: 'POST',
             data: ruta_editar,
-            beforeSend:function(){
-                editar_usuario.innerHTML="<center>Actualizando...</center>";
-                $('#loader_usuario').show();//mostrar LOADER
-            },//fin beforeSend
-            success: function(res){
-                    if(res == "ok")
-                    {
-                        swal('Perfecto', 'Se han actualizado los datos', 'success');
-                        $('#loader_usuario').hide();
-                        $('#btn_cerrarModalUsuario').click();
-                        cargar_datos_usuario();
-                    }//fin if
-                    if(res == "no_cambios")
-                    {
-                        swal('¿?', 'No realizaste ningun cambio', 'info');
-                        $('#btn_cerrarModalUsuario').click();
-                    }//fin if
-                    if(res == "error")
-                    {
-                        swal('Error', 'No se realizaron los cambios', 'error');
-                        $('#btn_cerrarModalUsuario').click();
-                    }//fin if
-            },//fin success
-            error: function(){
-                alert( "Error con el servidor" );
-            }//fin error
-        });//fin ajax
-    }
-    else{
-        swal('Datos incompletos', 'Por favor rellena todos los datos correctamente', 'warning');
+            beforeSend: function () {
+                editar_usuario.innerHTML = '<center>Actualizando...</center>';
+                $('#loader_usuario').show(); //mostrar LOADER
+            }, //fin beforeSend
+            success: function (res) {
+                if (res == 'ok') {
+                    swal('Perfecto', 'Se han actualizado los datos', 'success');
+                    $('#loader_usuario').hide();
+                    $('#btn_cerrarModalUsuario').click();
+                    cargar_datos_usuario();
+                } //fin if
+                if (res == 'no_cambios') {
+                    swal('¿?', 'No realizaste ningun cambio', 'info');
+                    $('#btn_cerrarModalUsuario').click();
+                } //fin if
+                if (res == 'error') {
+                    swal('Error', 'No se realizaron los cambios', 'error');
+                    $('#btn_cerrarModalUsuario').click();
+                } //fin if
+            }, //fin success
+            error: function () {
+                alert('Error con el servidor');
+            }, //fin error
+        }); //fin ajax
+    } else {
+        swal(
+            'Datos incompletos',
+            'Por favor rellena todos los datos correctamente',
+            'warning'
+        );
         return;
     }
+} // fin metodo actualizar_usuario
 
-}// fin metodo actualizar_usuario
+// Subir imagen
+$(document).ready(function () {
+    $('#btn_subirImagen').click(function () {
+        var fd = new FormData();
+        var files = $('#fotografia_georreferenciada')[0].files[0];
+        var nota = $('#nota').val();
+        fd.append('file', files);
+        fd.append('nota', nota);
+
+        $.ajax({
+            url: 'php_read_exif.php',
+            type: 'post',
+            data: fd,
+            contentType: false,
+            processData: false,
+            success: function (response) {
+                if (response != 0) {
+                    console.log(response);
+                    if (response == 3) {
+                        swal(
+                            'Perfecto',
+                            'Se ha subido la fotografia al sistema',
+                            'success'
+                        );
+
+                        $('#modal_subir_fotoGeo').modal('toggle');
+                        $('#foto_geo').trigger('reset');
+                    }
+                    if (response == 2) {
+                        swal(
+                            'Error',
+                            'No se pudo extraer información exif de la imagen',
+                            'error'
+                        );
+                    }
+                    if (response == 1) {
+                        swal(
+                            'Error',
+                            'No se pudo subir la fotografia al sistema',
+                            'error'
+                        );
+                    }
+                    if (response == 0) {
+                        swal('Error', 'La extension no es valida', 'error');
+                    }
+                } else {
+                    alert('file not uploaded');
+                }
+            },
+        });
+    });
+});
---
+++ usuario/php_read_exif.php
@@ -0,0 +1,147 @@
+<?php
+
+session_start();
+$usuario_session = $_SESSION['usuarioSession'];
+include "../conexion.php";
+date_default_timezone_set('America/Mexico_City');
+
+/* Getting file name && note */
+$filename = $_FILES['file']['name'];
+$nota = $_POST['nota'];
+
+/* Location */
+$imageFileType = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+$location = "fotografias_georreferenciadas/" . $usuario_session . '_' . date('Ymd_His') . '.' . $imageFileType;
+$uploadOk = 1;
+
+
+/* Valid Extensions */
+$valid_extensions = array("jpg", "jpeg", "png");
+/* Check file extension */
+if (!in_array(strtolower($imageFileType), $valid_extensions)) {
+    $uploadOk = 0;
+}
+
+if ($uploadOk == 0) {
+    echo 0;
+} else {
+    /* Upload file */
+    if (move_uploaded_file($_FILES['file']['tmp_name'], $location)) {
+        $fileGPS = f_getCoord($location);
+        if (!$fileGPS) {
+            unlink($location);
+            echo 2;
+            return;
+        }
+        // Obtener timestamp de exif
+        $exifTs = getTimeStamp($location);
+        // Obtuvo coordenadas
+        $data = (object) ['id_foto' => date('YmdHis', strtotime($exifTs)) . '_' . date('YmdHis'), 'nombre' => $usuario_session . '_' . date('Ymd_His'), 'notas' => $nota, 'url_path' => 'usuario/' . $location, 'datime_exi' => $exifTs, 'datime_upl' => date('Y-m-d H:i:s'), 'usuario' => $usuario_session, 'lat' => $fileGPS[0], 'lon' => $fileGPS[1]];
+
+        $query = "INSERT INTO opb_fotos_dgc_4326u(id_foto, nombre, notas, url_path, datime_exi, datime_upl, usuario, geom) VALUES ('$data->id_foto', '$data->nombre', '$data->notas', '$data->url_path', '$data->datime_exi', '$data->datime_upl', '$data->usuario', 
+        ST_SetSRID(ST_MakePoint($data->lon, $data->lat),4326))";
+
+        $result = pg_query($conexion, $query);
+
+        echo 3;
+        // echo json_encode($filesGPS[0]);
+        // echo 3;
+    } else {
+        echo 1;
+    }
+}
+
+function f_getGps($exifCoord, $hemi)
+{
+    $degrees = count($exifCoord) > 0 ? f_gps2Num($exifCoord[0]) : 0;
+    $minutes = count($exifCoord) > 1 ? f_gps2Num($exifCoord[1]) : 0;
+    $seconds = count($exifCoord) > 2 ? f_gps2Num($exifCoord[2]) : 0;
+    $flip = ($hemi == 'W' or $hemi == 'S') ? -1 : 1;
+    return $flip * ($degrees + $minutes / 60 + $seconds / 3600);
+}
+
+//Found on stackoverflow.com
+//Explodes the GPS string into usable float numbers (1° 2' 3.4'' => 1,2,3.4)
+function f_gps2Num($coordPart)
+{
+    $parts = explode('/', $coordPart);
+    if (count($parts) <= 0)
+        return 0;
+    if (count($parts) == 1)
+        return $parts[0];
+    return floatval($parts[0]) / floatval($parts[1]);
+}
+
+//Get a file's geolocalisation
+function f_getCoord($v_currFileLoc)
+{
+    //Read the file's exif data
+    $exif = exif_read_data($v_currFileLoc);
+
+    if (array_key_exists('GPSTimeStamp', $exif)) {
+        $exifTs = $exif['GPSTimeStamp'];
+    }
+    //Check that the wanted data exists
+    if (array_key_exists('GPSLongitude', $exif) && array_key_exists('GPSLatitude', $exif)) {
+        //If the data exists, generate the coordinates into an array
+        $photoCoord = array(f_getGps($exif["GPSLatitude"], $exif['GPSLatitudeRef']), f_getGps($exif["GPSLongitude"], $exif['GPSLongitudeRef']));
+        return $photoCoord;
+    } else {
+        //If no data exists, return false
+        return false;
+    }
+}
+
+//Get a multidim array containing: folder, filename, lat, long
+//Usage: $my_dir = 'fotos';
+// print_r(f_fileArray($my_dir));
+function f_fileArray($dir)
+{
+    $filesArray = array(); //Init the arrays
+    if (is_dir($dir)) { //Only proceed if this is really a folder
+        if ($dh = opendir($dir)) { //Open the folder
+            while (($file = readdir($dh)) !== false) { //Read all objects in the folder
+                if (substr($file, 0, 1) !== '.') { //Do not take the files beginning with a period (.)
+                    $v_Coord = f_getCoord($dir . '/' . $file); //Get the coordinates' array for each file
+                    if ($v_Coord !== false) { //Proceed only if the file has usable GPS data
+                        $filesArray[] = array("folder" => $dir, "file" => $file, "lat" => $v_Coord[0], "long" => $v_Coord[1]); //Create an array of array containing all info
+                    }
+                } //End if (substr())
+            } //End while loop
+            closedir($dh); //Close the folder
+        } //End if opendir
+        return $filesArray; //Return array of folder, file
+    } else {
+        return false; //Return FALSE if it's not a folder
+    } //End if is_dir
+}
+
+//Get an array of the map boundaries
+//Usage: $my_dir = 'fotos';
+// print_r(f_getMapBoundaries(f_fileArray($my_dir)));
+function f_getMapBoundaries($coordArray)
+{
+    $mapArray = array();
+    //This returns the whole array havng the min lat
+    $lat_min = $coordArray[array_search(min($lat = array_column($coordArray, 'lat')), $lat)];
+    //This returns the whole array havng the max lat
+    $lat_max = $coordArray[array_search(max($lat = array_column($coordArray, 'lat')), $lat)];
+    //This returns the whole array havng the min long
+    $long_min = $coordArray[array_search(min($long = array_column($coordArray, 'long')), $long)];
+    //This returns the whole array havng the max long
+    $long_max = $coordArray[array_search(max($long = array_column($coordArray, 'long')), $long)];
+    //Create an array having only lat_min,long_min,lat_max,long_max
+    $mapArray = array($lat_min['lat'], $long_min['long'], $lat_max['lat'], $long_max['long']);
+    return $mapArray;
+}
+
+function getTimeStamp($v_currFileLoc)
+{
+    //Read the file's exif data
+    $exif = exif_read_data($v_currFileLoc);
+
+    if (array_key_exists('DateTimeOriginal', $exif)) {
+        return $exif['DateTimeOriginal'];
+    } else
+        return false;
+}
